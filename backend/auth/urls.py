fcc62c875dd86b157295de262403fa9754e53571
Paulo Armando <paulovictorarmando@gmail.com>
2026-05-02 11:18:35 +0100

    urls.py updated

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UsuarioViewSet
+from rest_framework.routers import DefaultRouter
+
+routers = DefaultRouter()
+routers.register('auth/users', UsuarioViewSet, basename='users')
 
 urlpaterns = [
     path('auth/login/', TokenObtainPairView.as_view()),
-
+    path('auth/refresh/', TokenRefreshView.as_view()),
+    *routers.urls,
 ]
